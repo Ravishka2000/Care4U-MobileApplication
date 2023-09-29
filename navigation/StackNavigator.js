1ebc00237406948ddb962b1bbee3dfb36ff82a3a
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-09-29 13:00:04 +0530

    Create the Bottom Navigator

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,99 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+const StackNavigator = () => {
+    const Stack = createNativeStackNavigator();
+    const Tab = createBottomTabNavigator();
+
+    function BottomTabs() {
+        return (
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        tabBarLabel: "Home",
+                        tabBarLabelStyle: { color: "#435334" },
+                        tabBarIcon: ({ focused }) =>
+                            focused ? (
+                                <Ionicons name="ios-home" size={24} color="#435334" />
+                            ) : (
+                                <Ionicons
+                                    name="ios-home-outline"
+                                    size={24}
+                                    color="#435334"
+                                />
+                            ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Bookings"
+                    component={HomeScreen}
+                    options={{
+                        tabBarLabel: "Bookings",
+                        tabBarLabelStyle: { color: "#435334" },
+                        tabBarIcon: ({ focused }) =>
+                            focused ? (
+                                <Ionicons
+                                    name="ios-calendar"
+                                    size={24}
+                                    color="#435334"
+                                />
+                            ) : (
+                                <Ionicons
+                                    name="ios-calendar-outline"
+                                    size={24}
+                                    color="#435334"
+                                />
+                            ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={HomeScreen}
+                    options={{
+                        tabBarLabel: "Profile",
+                        tabBarLabelStyle: { color: "#435334" },
+                        tabBarIcon: ({ focused }) =>
+                            focused ? (
+                                <Ionicons
+                                    name="ios-person"
+                                    size={24}
+                                    color="#435334"
+                                />
+                            ) : (
+                                <Ionicons
+                                    name="ios-person-outline"
+                                    size={24}
+                                    color="#435334"
+                                />
+                            ),
+                    }}
+                />
+            </Tab.Navigator>
+        );
+    }
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+
+                <Stack.Screen
+                    name="Main"
+                    component={BottomTabs}
+                    options={{ headerShown: false }}
+                />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
