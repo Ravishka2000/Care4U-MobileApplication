cab748081a0fee51d440530ffbfe3faf422552e0
Ruwanara H D C <53680820+IT21005862@users.noreply.github.com>
2023-10-31 21:35:31 +0530

    create use states

--- screens/CareTakerHomeScreen.js
+++ screens/CareTakerHomeScreen.js
@@ -15,3 +15,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import jwt_decode from "jwt-decode";
 import axios from "axios";
 import AppLogo from "../assets/Care4U.png";
+
+const CareTakerHomeScreen = () => {
+    const { userId, setUserId } = useContext(UserType);
+    const [isLoading, setIsLoading] = useState(true);
+    const [bookings, setBookings] = useState([]);
+    const [pendingBookings, setPendingBookings] = useState([]);
+    const [acceptedBookings, setAcceptedBookings] = useState([]);
