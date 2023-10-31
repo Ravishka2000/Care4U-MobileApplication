3b179a9c4a132f313c7a5e52fe97540f5b388dc2
Ruwanara H D C <53680820+IT21005862@users.noreply.github.com>
2023-10-31 21:34:19 +0530

    adding imports

--- screens/CareTakerHomeScreen.js
+++ screens/CareTakerHomeScreen.js
@@ -1,1 +1,17 @@
-
+import React, { useContext, useEffect, useState } from "react";
+import {
+    ActivityIndicator,
+    Pressable,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+    SafeAreaView,
+    Image,
+} from "react-native";
+import { UserType } from "../UserContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+import AppLogo from "../assets/Care4U.png";
