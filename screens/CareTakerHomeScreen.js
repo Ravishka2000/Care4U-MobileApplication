6ee710c99924617b2a1a624f60e8f9d75b020c54
Ruwanara H D C <53680820+IT21005862@users.noreply.github.com>
2023-10-31 21:36:47 +0530

    add F/E routes

--- screens/CareTakerHomeScreen.js
+++ screens/CareTakerHomeScreen.js
@@ -22,3 +22,36 @@ const CareTakerHomeScreen = () => {
     const [bookings, setBookings] = useState([]);
     const [pendingBookings, setPendingBookings] = useState([]);
     const [acceptedBookings, setAcceptedBookings] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const token = await AsyncStorage.getItem("authToken");
+            const decodedToken = jwt_decode(token);
+            const userId = decodedToken.userId;
+            setUserId(userId);
+        };
+
+        fetchData();
+    }, []);
+
+    const fetchBookings = async () => {
+        try {
+            const response = await axios.get(
+                `https://care4u.onrender.com/api/booking`
+            );
+            const allBookings = response.data;
+            setBookings(allBookings);
+            const pending = allBookings.filter(
+                (booking) => booking.status === "Pending"
+            );
+            const accepted = allBookings.filter(
+                (booking) => booking.status === "Confirmed"
+            );
+            setPendingBookings(pending);
+            setAcceptedBookings(accepted);
+            setIsLoading(false);
+        } catch (error) {
+            console.error("Failed to fetch bookings:", error);
+            setIsLoading(false);
+        }
+    };
