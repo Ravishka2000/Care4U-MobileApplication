ebc9b20d2090306c197211f597f2a03de867f465
Pravini Wick <pravinipw15@gmail.com>
2023-10-31 19:49:23 +0530

    Update HomeScreen..js

--- screens/HomeScreen..js
+++ screens/HomeScreen..js
@@ -50,3 +50,78 @@ const HomeScreen = () => {
     const handleCaretakerPress = (caretakerId) => {
         navigation.navigate("ViewCaretaker", { caretakerId });
     };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Image source={AppLogo} style={styles.appLogo} />
+                <Text style={styles.appName}>CARE4U</Text>
+            </View>
+
+            <Text style={styles.pageTitle}>Browse Caretakers</Text>
+
+            <View style={styles.filterContainer}>
+                <Text style={styles.filterLabel}>Filter by Speciality:</Text>
+                <ModalDropdown
+                    options={specialities}
+                    defaultValue={specialities[0]}
+                    textStyle={styles.filterInput}
+                    style={styles.dropdown}
+                    dropdownStyle={styles.dropdownMenu}
+                    dropdownTextStyle={styles.dropdownText}
+                    onSelect={(index, value) => setFilter(value)}
+                />
+                <Pressable
+                    style={styles.applyFilterButton}
+                    onPress={applyFilter}
+                >
+                    <Text style={styles.applyFilterButtonText}>
+                        Apply Filter
+                    </Text>
+                </Pressable>
+            </View>
+
+            {isLoading ? (
+                <ActivityIndicator
+                    size="large"
+                    color="#2E86DE"
+                    style={styles.loadingIndicator}
+                />
+            ) : (
+                <ScrollView>
+                    {caretakers.map((caretaker, index) => (
+                        <Pressable
+                            key={caretaker._id}
+                            style={styles.caretakerCard}
+                            onPress={() => handleCaretakerPress(caretaker._id)}
+                        >
+                            <View style={styles.caretakerImageContainer}>
+                                <Image
+                                    source={{ uri: caretaker.image }}
+                                    style={styles.caretakerImage}
+                                />
+                            </View>
+                            <View style={styles.caretakerDetails}>
+                                <Text style={styles.caretakerName}>
+                                    {caretaker.user.firstName}{" "}
+                                    {caretaker.user.lastName}
+                                </Text>
+                                <Text style={styles.caretakerSpeciality}>
+                                    Speciality: {caretaker.speciality}
+                                </Text>
+                                <View style={styles.caretakerInfo}>
+                                    <Text style={styles.caretakerRate}>
+                                        Age: {caretaker.user.age} years
+                                    </Text>
+                                    <Text style={styles.caretakerRate}>
+                                        Rate: Rs.{caretaker.hourlyRate}
+                                    </Text>
+                                </View>
+                            </View>
+                        </Pressable>
+                    ))}
+                </ScrollView>
+            )}
+        </SafeAreaView>
+    );
+};
